9286895569a17ea8c15c50c2fc49764dd944db97
Deep <dshreyaskar@gmail.com>
2025-09-06 11:39:48 +0530

    third commit

--- p1/a1/urls.py
+++ p1/a1/urls.py
@@ -3,5 +3,6 @@ from django.urls import path,include
 from .views import *
 
 urlpatterns = [
-    path("",home)
+    path("",home),
+    path("success",sucess)
 ]
--- p1/a1/views.py
+++ p1/a1/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 def home(req):
     return render(req,"home.html")
 # Create your views here.
+def sucess(req):
+    return render(req,"success.html")
